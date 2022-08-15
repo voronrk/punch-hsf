236a23deb0932500cfe17a547b431324cae167d3
voronrk <voron_76@mail.ru>
2022-08-15 07:02:17 +0300

    Add AuthController and route for it

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\MachineController;
 use App\Http\Controllers\MaterialController;
 use App\Http\Controllers\PunchController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,40 +20,38 @@ use App\Http\Controllers\PunchController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::middleware('auth:api')->group(function () {
+    Route::any('/machine.add', [MachineController::class, 'store']);
+    Route::any('/material.add', [MaterialController::class, 'store']);
+    Route::any('/product.add', [ProductController::class, 'store']);
+    Route::any('/punch.add', [PunchController::class, 'store']);
+
+    Route::any('/machine.update', [MachineController::class, 'update']);
+    Route::any('/material.update', [MaterialController::class, 'update']);
+    Route::any('/product.update', [ProductController::class, 'update']);
+    Route::any('/punch.update', [PunchController::class, 'update']);
+
+    Route::any('/machine.delete', [MachineController::class, 'destroy']);
+    Route::any('/material.delete', [MaterialController::class, 'destroy']);
+    Route::any('/product.delete', [ProductController::class, 'destroy']);
+    Route::any('/punch.delete', [PunchController::class, 'destroy']);
+
+    Route::any('/machine.get', [MachineController::class, 'show']);
+    Route::any('/material.get', [MaterialController::class, 'show']);
+    Route::any('/product.get', [ProductController::class, 'show']);
+    Route::any('/punch.get', [PunchController::class, 'show']);
+
+    Route::any('/machine.list', [MachineController::class, 'index']);
+    Route::any('/material.list', [MaterialController::class, 'index']);
+    Route::any('/product.list', [ProductController::class, 'index']);
+    Route::any('/punch.list', [PunchController::class, 'index']);
+
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 });
 
-Route::any('/machine.add', [MachineController::class, 'store']);
-Route::any('/material.add', [MaterialController::class, 'store']);
-Route::any('/product.add', [ProductController::class, 'store']);
-Route::any('/punch.add', [PunchController::class, 'store']);
-
-Route::any('/machine.update', [MachineController::class, 'update']);
-Route::any('/material.update', [MaterialController::class, 'update']);
-Route::any('/product.update', [ProductController::class, 'update']);
-Route::any('/punch.update', [PunchController::class, 'update']);
-
-Route::any('/machine.delete', [MachineController::class, 'destroy']);
-Route::any('/material.delete', [MaterialController::class, 'destroy']);
-Route::any('/product.delete', [ProductController::class, 'destroy']);
-Route::any('/punch.delete', [PunchController::class, 'destroy']);
-
-Route::any('/machine.get', [MachineController::class, 'show']);
-Route::any('/material.get', [MaterialController::class, 'show']);
-Route::any('/product.get', [ProductController::class, 'show']);
-Route::any('/punch.get', [PunchController::class, 'show']);
-
-Route::any('/machine.list', [MachineController::class, 'index']);
-Route::any('/material.list', [MaterialController::class, 'index']);
-Route::any('/product.list', [ProductController::class, 'index']);
-Route::any('/punch.list', [PunchController::class, 'index']);
-
-
-// Route::apiResources([
-//     'product' => ProductController::class,
-//     'machine' => MachineController::class,
-//     'material' => MaterialController::class,
-//     'punch' => PunchController::class,
-// ]);
-
+Route::get('/register', [AuthController::class, 'register'])->name('register');
+Route::get('/login', [AuthController::class, 'login'])->name('login');
